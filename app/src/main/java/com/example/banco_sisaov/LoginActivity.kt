ffdb2ef751f275547adb2af201d4ec3e51f1cb11
SilviTorre <silveela@gmail.com>
2024-11-02 20:21:27 +0100

    Nou icono de app, configuracio novo de la alerta dels camps del login en el text field ja programat y validat per lenght

--- app/src/main/java/com/example/banco_sisaov/LoginActivity.kt
+++ app/src/main/java/com/example/banco_sisaov/LoginActivity.kt
@@ -8,7 +8,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.banco_sisaov.databinding.ActivityLoginBinding
 import com.google.android.material.snackbar.Snackbar
-import kotlin.system.exitProcess
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding : ActivityLoginBinding
@@ -28,28 +27,31 @@ class LoginActivity : AppCompatActivity() {
         }
 
         binding.tietUsr.setOnFocusChangeListener { v, hasFocus ->
-            if (!hasFocus && binding.tietUsr.text!!.isEmpty()){
-                Snackbar.make(binding.root, "Debes introducir el usuario!", Snackbar.LENGTH_SHORT).show()
+            if (!hasFocus && binding.tietUsr.text.toString().length != 9){
+                binding.tfUsr.error = "Debe ser un DNI de 8 numeros y una letra"
+            }
+            else{
+                binding.tfUsr.error = null
             }
         }
-
 
         binding.tietPsw.setOnFocusChangeListener { v, hasFocus ->
-            if (!hasFocus && binding.tietPsw.text!!.isEmpty()){
-                Snackbar.make(binding.root, "Debes introducir la contrasenya!", Snackbar.LENGTH_SHORT).show()
+            if (!hasFocus && binding.tietPsw.text.toString().length != 8){
+                binding.tfPsw.error = "Debe contener almenos 8 caracteres"
+            }
+            else {
+                binding.tfPsw.error = null
             }
         }
 
-
         binding.btEnter.setOnClickListener {
-            if(binding.tietUsr.text!!.isNotEmpty() && binding.tietPsw.text!!.isNotEmpty()){
+            if(binding.tfUsr.error == null && binding.tfPsw.error == null){
                 val intent = Intent(this, MainActivity::class.java)
                 intent.putExtra("DNI", binding.tietUsr.text.toString())
                 startActivity(intent)
             }
             else Snackbar.make(binding.root, "Debes rellenar los campos obligatorios", Snackbar.LENGTH_SHORT).show()
         }
-
 
         binding.btExit.setOnClickListener {
             finish()
